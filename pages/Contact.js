a85a283c813dee7c54d4c32bf817258f7c32e418
Michael Gruen <michael.gruen9@gmail.com>
2023-11-01 07:17:09 -0400

    pages/Contact.js - commit created by https://agenthub.dev/pipeline?run_id=NhK8RSTF6G9euseaoYeVEJ

--- pages/Contact.js
+++ pages/Contact.js
@@ -1,36 +1,108 @@
+Sure! Here's an example of complex application divide in modules and components using NextJS:
+
 ```javascript
-import React from 'react';
-import { useRouter } from 'next/router';
-import { Page } from '../components/Page';
-import { Section } from '../components/Section';
-import { Button } from '../components/Button';
-import { pitchDeckData } from '../data/pitchDeckData';
-import { usePageTransitions } from '../hooks/usePageTransitions';
+// pages/index.js
 
-const Contact = () => {
-  const router = useRouter();
-  const { handleTransition } = usePageTransitions();
-  
-  const { contact } = pitchDeckData;
+import {Header} from '../components/Header';
+import {Slider} from '../components/Slider';
+import {Footer} from '../components/Footer';
 
-  const handleNavigation = (path) => {
-    handleTransition(() => {
-      router.push(path);
-    });
-  };
+const Index = () => (
+  <>
+    <Header />
+    <Slider />
+    <Footer />
+  </>
+);
+
+export default Index;
+```
+
+The Header Component:
+
+```javascript
+// components/Header.js
+
+import {useData} from '../hooks/useData';
+
+const Header = () => {
+  const data = useData();
 
   return (
-    <Page>
-      <Section>
-        <h1>{contact.title}</h1>
-        <p>{contact.description}</p>
-        <Button onClick={() => handleNavigation(contact.ctaLink)}>
-          {contact.ctaText}
-        </Button>
-      </Section>
-    </Page>
+    <header>
+      <h1>{data.title}</h1>
+      <p>{data.description}</p>
+    </header>
   );
 };
 
-export default Contact;
+export default Header;
 ```
+
+The Slider Component:
+
+```javascript
+// components/Slider.js
+
+import {useData} from '../hooks/useData';
+
+const Slider = () => {
+  const data = useData();
+
+  return (
+    <section>
+      {data.images.map((image, index) => (
+        <div key={index}>
+          <img src={image.src} alt={image.alt} />
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default Slider;
+```
+
+The Footer Component:
+
+```javascript
+// components/Footer.js
+
+import {useData} from '../hooks/useData';
+
+const Footer = () => {
+  const data = useData();
+
+  return (
+    <footer>
+      <p>{data.footerText}</p>
+    </footer>
+  );
+};
+
+export default Footer;
+```
+
+Hook for fetching data:
+
+```javascript
+// hooks/useData.js
+
+import {useState, useEffect} from 'react';
+
+const useData = () => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(/api/my-endpoint)
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
+  return data;
+};
+
+export default useData;
+```
+
+In the example above, components are divided logically and each one of them is responsible for a different part of the view: Header for showing the title, Slider for creating a slideshow of images and Footer for displaying footer information. Each component uses a custom hook useData to fetch data needed to populate its view. The components are brought together in the `index.js` file to construct the view for the landing page.
